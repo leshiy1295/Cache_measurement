d66bc9ae75403a8da6a92d39b7566519a83228f9
Alexey Halaidzhy <a.halaidzhy@corp.mail.ru>
2015-04-03 23:04:18 +0300

    Size of lists is on bytes for good interpretation of results. Removed elem_size Added padding for sequentional test

--- main.c
+++ main.c
@@ -9,40 +9,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #define TWICE(x) x x
 
-const size_t CYCLES_COUNT = 10000000;
+const size_t CYCLES_COUNT = 1000000;
 
 const size_t MIN_POW = 10;
 const size_t MAX_POW = 28;
 
-const size_t MIN_ELEM_SIZE = 1000;
-const size_t MAX_ELEM_SIZE = 40000;
-const size_t ELEM_SIZE_DELTA = 1000;
+const size_t MIN_PADDING = 1;
+const size_t MAX_PADDING = 40001;
+const size_t DELTA_PADDING = 5000;
 
-uint64_t rdtsc(){
-    unsigned int lo,hi;
-    __asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
-    return ((uint64_t)hi << 32) | lo;
-}
+const size_t MIN_LIST_SIZE = 1000 / sizeof(void *);
+const size_t MAX_LIST_SIZE = 5000000 / sizeof(void *);
+const double LIST_SIZE_STEP = 1.15;
 
-void build_conseq_list(void **list, size_t size) {
+void build_conseq_list(void **list, size_t size, size_t padding) {
     size_t i;
-    for (i = 0; i < size - 1; ++i) {
-        list[i] = list + i + 1;
+    for (i = 0; i < size - padding; i += padding) {
+        list[i] = list + i + padding;
     }
     list[i] = list;
 }
 
 void build_random_list(void **list, size_t size) {
+    for (size_t i = 0; i < size; ++i) {
+        list[i] = NULL;
+    }
     srand((unsigned int) time(NULL));
     size_t prev_index = 0;
     for (size_t i = 0; i < size - 1; ++i) {
-        size_t current_index;
-        do {
-            current_index = rand() % size;
-        } while (list[current_index]);
+        size_t current_index = rand() % size;
+        while (list[current_index]) {
+            current_index = (current_index + 1) % size;
+        }
         list[prev_index] = list + current_index;
         prev_index = current_index;
     }
@@ -53,37 +55,38 @@ double measure(void **list) {
     double avg;
     
     void **cur = list;
-    uint64_t start = rdtsc();
+    clock_t start = clock();
     for (size_t j = 0; j < CYCLES_COUNT; ++j) {
-        TWICE(TWICE(TWICE(TWICE(TWICE(TWICE(TWICE(TWICE( cur = (void**)*cur; ))))))))
+        TWICE(TWICE(TWICE(TWICE(TWICE(TWICE(TWICE(TWICE( cur = *cur; ))))))))
     }
-    uint64_t stop = rdtsc();
+    clock_t stop = clock();
     avg = (stop - start) * 1.0 / CYCLES_COUNT;
     
     return avg;
 }
 
 int main(int argc, char *argv[]) {
-    printf("Sequential access\n");
-    for (size_t elem_size = MIN_ELEM_SIZE; elem_size <= MAX_ELEM_SIZE; elem_size += ELEM_SIZE_DELTA) {
-        printf("ELEM_SIZE: %lu\n", elem_size);
-        for (size_t i = MIN_POW; i <= MAX_POW; ++i) {
-            size_t list_size = 1 << i;
-            void **list = (void **)calloc(list_size, elem_size * sizeof(void *));
-            build_conseq_list(list, list_size);
-            printf("POW: %lu, TICKS: %f\n", i, measure(list));
+    if (argc == 2 && !strcmp(argv[1], "-r")) {
+        printf("Random access\n");
+        for (size_t list_size = MIN_LIST_SIZE; list_size <= MAX_LIST_SIZE; list_size *= LIST_SIZE_STEP) {                void **list = (void **)calloc(list_size, sizeof(void *));
+            build_random_list(list, list_size);
+            printf("LIST WAS SUCCESSFULLY BUILT\n");
+            printf("SIZE: %lu, TICKS: %f\n", list_size * sizeof(void *), measure(list));
             free(list);
         }
     }
-    printf("Random access\n");
-    for (size_t elem_size = MIN_ELEM_SIZE; elem_size <= MAX_ELEM_SIZE; elem_size += ELEM_SIZE_DELTA) {
-        printf("ELEM_SIZE: %lu\n", elem_size);
-        for (size_t i = MIN_POW; i <= MAX_POW; ++i) {
-            size_t list_size = 1 << i;
-            void **list = (void **)calloc(list_size, elem_size * sizeof(void *));
-            build_random_list(list, list_size);
-            printf("POW: %lu, TICKS: %f\n", i, measure(list));
-            free(list);
+    else {
+        printf("Sequential access\n");
+        for (size_t padding = MIN_PADDING; padding <= MAX_PADDING; padding += DELTA_PADDING) {
+            printf("PADDING: %lu\n", padding);
+            for (size_t list_size = MIN_LIST_SIZE; list_size <= MAX_LIST_SIZE; list_size *= LIST_SIZE_STEP) {
+                if (list_size > padding) {
+                    void **list = (void **)calloc(list_size, sizeof(void *));
+                    build_conseq_list(list, list_size, padding);
+                    printf("LIST_SIZE: %lu, SIZE: %lu, TICKS: %f\n", list_size, list_size * sizeof(void *), measure(list));
+                    free(list);
+                }
+            }
         }
     }
     return 0;
